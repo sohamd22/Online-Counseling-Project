66be1755b8a3a4634321a34a4a593c33e93d8782
Soham Daga <soham.daga22@gmail.com>
2023-10-02 19:36:38 -0700

    Added signing up buttons

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Online Counseling Project - EPICS');
+    res.sendFile('./index.html',{root: __dirname });
 });
 
 const server = app.listen(1000, () => {
