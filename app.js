d50e372a27e96ddd377bfe5e9ad9041e798b6465
Soham Daga <soham.daga22@gmail.com>
2023-10-04 12:15:39 -0700

    Added initial auth files

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 const express = require("express");
 const app = express();
 
+const path = require('path')
+app.use('/public', express.static(path.join(__dirname, 'public')))
+
 app.get('/', (req, res) => {
     res.sendFile('./index.html',{root: __dirname });
+});
+
+app.get('/login-patient', (req, res) => {
+    res.sendFile('./user-auth.html',{root: __dirname });
+});
+
+app.get('/signup-patient', (req, res) => {
+    res.sendFile('./user-auth.html',{root: __dirname });
+});
+
+app.get('/login-professional', (req, res) => {
+    res.sendFile('./user-auth.html',{root: __dirname });
+});
+
+app.get('/signup-professional', (req, res) => {
+    res.sendFile('./user-auth.html',{root: __dirname });
 });
 
 const server = app.listen(1000, () => {
